3560d88c012ced40a4183c26ce8b655b82d44b51
Atmos945 <92547087+Atmos945@users.noreply.github.com>
2022-09-08 21:31:50 +0200

    Design bases, Java Installation page, misc

--- WOLFforce Installer/MainWindow.xaml.cs
+++ WOLFforce Installer/MainWindow.xaml.cs
@@ -22,20 +22,24 @@ namespace WOLFforce_Installer
     {
         public MainWindow()
         {
+            Uri iconUri = new Uri("pack://application:,,,/icon.ico", UriKind.RelativeOrAbsolute);
+            this.Icon = BitmapFrame.Create(iconUri);
             InitializeComponent();
-
         }
+
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            if (Bvn_basic.IsChecked == true)
-            {
-                MessageBox.Show("T pauvre");
-            }
-            else if (Bvn_ext.IsChecked == true)
-            {
-                MessageBox.Show("Niice");
-            }
+            /*            if (Bvn_basic.IsChecked == true)
+                        {
+                            MessageBox.Show("T pauvre");
+                        }
+                        else if (Bvn_ext.IsChecked == true)
+                        {
+                            MessageBox.Show("Niice");
+                        }*/
+
+            this.Content = new Java();
         }
     }
 
